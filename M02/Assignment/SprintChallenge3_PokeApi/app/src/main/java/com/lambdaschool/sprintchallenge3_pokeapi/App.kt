862bdbe5f3c2441d6421bea534548ca66a0beaa6
Justin Gent <justinbgent@gmail.com>
2019-10-01 15:54:01 -0600

    Add App class that extends Application. Add to manifest.

---
+++ M02/Assignment/SprintChallenge3_PokeApi/app/src/main/java/com/lambdaschool/sprintchallenge3_pokeapi/App.kt
@@ -0,0 +1,10 @@
+package com.lambdaschool.sprintchallenge3_pokeapi
+
+import android.app.Application
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
